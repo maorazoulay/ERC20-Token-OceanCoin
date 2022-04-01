c0dc617b69bec32b8cf64911b694676acdfce7ac
Maor <maorazoulay@gmail.com>
2022-04-01 14:53:12 -0400

    Edited coin name and symbol

--- contracts/OceanCoin.sol
+++ contracts/OceanCoin.sol
@@ -1,12 +1,12 @@
-// contracts/OurToken.sol
+// contracts/OceanCoin.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OurToken is ERC20 {
+contract OceanCoin is ERC20 {
     // @initialSupply in wei
-    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+    constructor(uint256 initialSupply) ERC20("OceanCoin", "OCN") {
         _mint(msg.sender, initialSupply);
     }
 }
